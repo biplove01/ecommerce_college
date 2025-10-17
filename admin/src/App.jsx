1196b6ecbab2004a270d172df97befa87f92b629
Laya Pandey <layapandey7171@gmail.com>
2025-10-18 00:09:15 +0545

    I'm dead inside

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Sidebar from './components/sidebar/sidebar'
+import Navbar from './components/Navbar/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="app">
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<div>Add Items Page</div>} />
+          <Route path="/list" element={<div>List Items Page</div>} />
+          <Route path="/orders" element={<div>Orders Page</div>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
